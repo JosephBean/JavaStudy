a95eb607a05de4554b9afef08cddb0896d3a5921
ShellFolder <kk1593@naver.com>
2024-05-16 15:22:19 +0900

    주석 알아보기

--- src/java0516/Study01.java
+++ src/java0516/Study01.java
@@ -3,14 +3,14 @@ package java0516;
 public class Study01 {
 
 	public static void main(String[] args) {
-		
+
 		int 숫자 = 1;
 		System.out.println(숫자 + "");
 		int 숫자2;
-		//System.out.println(숫자2 + "");
+		// System.out.println(숫자2 + "");
+/*
 		숫자2 = 숫자;
 		System.out.println(숫자2 + "");
-		
+*/
 	}
-
 }
