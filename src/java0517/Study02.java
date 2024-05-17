92a6d15ae71a2e35c3ef3b45229bc9d3b5fe3b7f
ShellFolder <kk1593@naver.com>
2024-05-17 15:24:58 +0900

    조건문 1

--- src/java0517/Study02.java
+++ src/java0517/Study02.java
@@ -10,6 +10,29 @@ public class Study02 {
 
 	public static void main(String[] args) {
 
+		boolean 논리값 = true;
+		/*
+		 * if(true) { System.out.println("참"); 논리값 = false; }
+		 * 
+		 * if(논리값) { System.out.println("논리값이 참이다."); }
+		 */
+		
+		if(!논리값) {
+			System.out.println("부정의 논리값");
+		}
+		
+		int a, b;
+		a = 5;		// 5;
+		b = ++a;	// 6;
+		
+		System.out.println(a + " : " + b);
+		
+		if(a == b) {
+			System.out.println("a와 b는 같다");
+		} else {
+			System.out.println("a와 b는 다르다.");
+		}
+		
 	}
 
 }
