e64addf786e621739413a27f89bfbff0d9fa9322
ShellFolder <kk1593@naver.com>
2024-05-20 11:27:32 +0900

    반복문 2

--- src/java0520/Study01.java
+++ src/java0520/Study01.java
@@ -9,8 +9,15 @@ public class Study01 {
 
 	public static void main(String[] args) {
 		
+		int tot = 0;
 		
+		for (int i = 1; i <= 10 ; i++) { // i >> (1 > 2 > 3 > 4 > 5 > 6 > 7 > 8 > 9 > 10) 
+//			System.out.println("전 : " + i);
+			tot += i;
+//			System.out.println("후 : " + i);
+		}
 		
+		System.out.println("반복문의 합 : " + tot);
 	}
 
 }
---
+++ src/java0520/Study02.java
@@ -0,0 +1,23 @@
+package java0520;
+
+public class Study02 {
+
+	public static void main(String[] args) {
+		
+		// 구구단
+		// 단 * 증가값 = 결과값
+		
+		for (int 단 = 1; 단 <= 1; 단++) { // 단를 위한 반복문
+			
+			for(int 증가값 = 1; 증가값 <= 9; 증가값++ ) { // 증가값 위한 반복문 1 ~ 9 
+				
+				// 출력 하는 곳
+				System.out.println(단 + " * " + 증가값 + " = " + (단 * 증가값));
+				
+			}
+			
+		}
+
+	}
+
+}
