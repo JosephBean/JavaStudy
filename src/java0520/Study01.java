6fa2a0ac37234354b4740c43838fedcf0b76e854
ShellFolder <kk1593@naver.com>
2024-05-20 13:08:45 +0900

    반복문 4

--- src/java0520/Study01.java
+++ src/java0520/Study01.java
@@ -9,6 +9,11 @@ public class Study01 {
 
 	public static void main(String[] args) {
 		
+		for( ; ; ) {
+			System.out.println("반복문");
+			break;
+		}
+		
 		int tot = 0;
 		
 		for (int i = 1; i <= 10 ; i++) { // i >> (1 > 2 > 3 > 4 > 5 > 6 > 7 > 8 > 9 > 10) 
