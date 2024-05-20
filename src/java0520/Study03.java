1653aabe4d210c18205db936b6598f706ce539fa
ShellFolder <kk1593@naver.com>
2024-05-20 14:57:43 +0900

    반복문 5

--- src/java0520/Study03.java
+++ src/java0520/Study03.java
@@ -9,8 +9,20 @@ package java0520;
 public class Study03 {
 
 	public static void main(String[] args) {
-
+		int s = 0;
+		int a = 4;
+		while(s < a) {
+//			System.out.print(s + "\t");
+//			System.out.print(a + "\t");
+//			System.out.println(s >= a);
+//			if(s >= a) break;  // 검사 횟수 
+//			System.out.println("반복문");
+			s++;
+		}
 		
+		do {
+			System.out.println("반복문");
+		} while(a < s);
 		
 	}
 
