f7c8b7088338a65ee01aa7ba7c21ce23f2e237c0
ShellFolder <kk1593@naver.com>
2024-05-21 12:14:01 +0900

    1차 배열 문제 2

---
+++ src/java0521/Study03.java
@@ -0,0 +1,30 @@
+package java0521;
+
+public class Study03 {
+
+	public static void main(String[] args) {
+
+		// 1차원 배열 (행) [행, 세로]
+		// 0    = 100  <<- 배열[0] = 100;
+		// 1    = 0
+		// 2    = 0
+		// 3    = 0
+		
+		int[] 배열 = new int[10];
+		//int 배열2[];
+		
+		int 크기 = 배열.length;
+		System.out.println(크기);
+		
+		배열[0] = 100; // 0 ~ 9
+		System.out.println(배열[0]);
+		
+		// 2차원 배열 [행, 세로][열, 가로]
+		// [행0, 열0], [행0, 열1] 
+		// [행1, 열0]
+		// [행2, 열0], [행2, 열1], [행2, 열2]
+		// [행3, 열0]
+		
+	}
+
+}
