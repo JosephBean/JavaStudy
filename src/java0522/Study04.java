155fd7092ab0f59b0c770bdf155bc2900cfc9988
ShellFolder <kk1593@naver.com>
2024-05-22 11:51:17 +0900

    별 출력 문제4 풀이

--- src/java0522/Study04.java
+++ src/java0522/Study04.java
@@ -19,8 +19,35 @@ public class Study04 {
 	    String 별 = "★";
 	
 	    /*** 코드 시작 ***/
+	    String[][] 배열 = new String[5][5];
+	    
+	    for(int i = 0; i < 배열.length; i++) {
+	    	for(int j = 0; j < 배열[i].length; j++) {
+	    		String 빈공간 = " ";
+	    		if(i == 0 && j >= 4) {
+	    			배열[i][j] = 별;
+	    		} else if(i == 1 && j >= 3) {
+	    			배열[i][j] = 별;
+	    		} else if(i == 2 && j >= 2) {
+	    			배열[i][j] = 별;
+	    		} else if(i == 3 && j >= 1) {
+	    			배열[i][j] = 별;
+	    		} else if(i == 4 && j >= 0) {
+	    			배열[i][j] = 별;
+	    		} else {
+	    			배열[i][j] = 빈공간;
+	    		}
+	    	}
+	    }
+	    
+	    
 	
-	
+	    for(int i = 0; i < 배열.length; i++) {
+	    	for(int j = 0; j < 배열[i].length; j++) {
+	    		System.out.print(배열[i][j]);
+	    	}
+	    	System.out.println();
+	    }
 	
 	    /*** 코드 종료 ***/
 
