6ca6c77ed6432bee72c312238b773d1d1f925807
ShellFolder <kk1593@naver.com>
2024-05-22 12:54:13 +0900

    별 출력 문제5-7 풀이

--- src/java0522/Study05.java
+++ src/java0522/Study05.java
@@ -19,8 +19,32 @@ public class Study05 {
 	    String 별 = "★";
 	
 	    /*** 코드 시작 ***/
-	
-	
+	    String[][] 배열 = new String[5][9];
+	    
+	    for(int i = 0; i < 배열.length; i++) {
+	    	for(int j = 0; j < 배열[i].length; j++) {
+	    		String 빈공간 = " ";
+	    		if(i == 1 && (j <= 0 || j >= 8 )) {
+	    			배열[i][j] = 빈공간;
+	    		} else if(i == 2 && (j <= 1 || j >= 7 )) {
+	    			배열[i][j] = 빈공간;
+	    		} else if(i == 3 && (j <= 2 || j >= 6 )) {
+	    			배열[i][j] = 빈공간;
+	    		} else if(i == 4 && (j <= 3 || j >= 5 )) {
+	    			배열[i][j] = 빈공간;
+	    		} else {
+	    			배열[i][j] = 별;
+	    		}
+	    	}
+	    }
+
+		
+	    for(int i = 0; i < 배열.length; i++) {
+	    	for(int j = 0; j < 배열[i].length; j++) {
+	    		System.out.print(배열[i][j]);
+	    	}
+	    	System.out.println();
+	    }
 	
 	    /*** 코드 종료 ***/
 
--- src/java0522/Study07.java
+++ src/java0522/Study07.java
@@ -23,8 +23,32 @@ public class Study07 {
 	    String 별 = "★";
 	
 	    /*** 코드 시작 ***/
-	
-	
+	    String[][] 배열 = new String[9][5];
+	    
+	    for(int i = 0; i < 배열.length; i++) {
+	    	for(int j = 0; j < 배열[i].length; j++) {
+	    		String 빈공간 = " ";
+	    		if((i == 1 || i == 7) && j >= 4) {
+	    			배열[i][j] = 빈공간;
+	    		} else if((i == 2 || i == 6) && j >= 3) {
+	    			배열[i][j] = 빈공간;
+	    		} else if((i == 3 || i == 5) && j >= 2) {
+	    			배열[i][j] = 빈공간;
+	    		} else if(i == 4 && j >= 1) {
+	    			배열[i][j] = 빈공간;
+	    		} else {
+	    			배열[i][j] = 별;
+	    		}
+	    	}
+	    }
+	    
+	    
+	    for(int i = 0; i < 배열.length; i++) {
+	    	for(int j = 0; j < 배열[i].length; j++) {
+	    		System.out.print(배열[i][j]);
+	    	}
+	    	System.out.println();
+	    }
 	
 	    /*** 코드 종료 ***/
 
