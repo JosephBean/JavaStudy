4078552e8ec93a556342f54fe7a835e504ce27d8
ShellFolder <kk1593@naver.com>
2024-05-22 11:00:44 +0900

    별 출력 문제2 풀이

--- src/java0522/Study02.java
+++ src/java0522/Study02.java
@@ -19,7 +19,36 @@ public class Study02 {
 	    String 별 = "★";
 	
 	    /*** 코드 시작 ***/
+	    String[][] 배열 = new String[5][5];
 	    
+	    for(int i = 0; i < 배열.length; i++) {
+	    	for(int j = 0; j < 배열[i].length; j++) {
+	    		if(i == 0 && j <= 0) {
+	    			배열[i][j] = 별;
+	    		} else if (i == 1 && j <= 1) {
+	    			배열[i][j] = 별;
+	    		} else if (i == 2 && j <= 2) {
+	    			배열[i][j] = 별;
+	    		} else if (i == 3 && j <= 3) {
+	    			배열[i][j] = 별;
+	    		} else if (i == 4 && j <= 4) {
+	    			배열[i][j] = 별;
+	    		} else {
+	    			배열[i][j] = "";
+	    		}
+	    	}
+	    }
+	    
+	    
+	    
+	    
+	    
+	    for(int i = 0; i < 배열.length; i++) {
+	    	for(int j = 0; j < 배열[i].length; j++) {
+	    		System.out.print(배열[i][j]);
+	    	}
+	    	System.out.println();
+	    }
 	
 	
 	    /*** 코드 종료 ***/
