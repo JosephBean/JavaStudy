3014cb6367a14eb59725a216aca00aa04ff495c5
ShellFolder <kk1593@naver.com>
2024-05-23 12:42:17 +0900

    Scanner 알아보기 5 풀이

--- src/java0523/Study05.java
+++ src/java0523/Study05.java
@@ -15,11 +15,43 @@ public class Study05 {
 		Scanner sc = new Scanner(System.in);
 		
 		/** 객체 사용 **/
+		String 더하기 = "+";
+		String 빼기  = "-";
+		String 나누기 = "/";
+		String 곱하기 = "*";
 		
-		
+		while(true) {
+			System.out.println("첫번째 숫자를 입력하세요.");
+			int a = sc.nextInt();
+			
+			int b = sc.nextInt();
+			
+			System.out.println("두번째 숫자를 입력하세요.");
+			int c = sc.nextInt();
+			
+			String d = "";
+			switch (b) {
+				case 1:
+					d = a + 더하기 + c + "=" + (a + c);
+					break;
+				case 2:
+					d = a + 빼기 + c + "=" + (a - c);
+					break;
+				case 3:
+					d = a + 나누기 + c + "=" + (a / c);
+					break;
+				case 4:
+					d = a + 곱하기 + c + "=" + (a * c);
+					break;
+				default:
+					
+			}
+			
+			System.out.println(d);
+		}
 		
 		/** 객체 제거 **/
-		sc.close();
+		//sc.close();
 		
 	}
 
