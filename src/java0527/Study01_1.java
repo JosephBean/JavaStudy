371747be273000ea215625cc9a687ab51d2d1c0b
ShellFolder <kk1593@naver.com>
2024-05-27 15:04:36 +0900

    JDBC 테이블 생성 및 데이터 넣기

--- src/java0527/Study01_1.java
+++ src/java0527/Study01_1.java
@@ -1,25 +1,35 @@
 package java0527;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Study01_1 {
+	
+	String 드라이버 = "org.mariadb.jdbc.Driver";
+	String 주소 = "jdbc:mariadb://localhost:3306/edu";
+	String 사용자 = "root";
+	String 비밀번호 = "1234";
 
 	public void 접속() {
 		System.out.println("접속 시작!");
-		String 드라이버 = "org.mariadb.jdbc.Driver";
 		try {
 			Class.forName(드라이버);
-			System.out.println("드라이버 클래스 존재 합니다.");
+			Connection conn = DriverManager.getConnection(주소, 사용자, 비밀번호);
 			
-			// 데이터베이스 접속 
-			// jdbc > 데이터베이스타입 > 호스트(IP) > 포트 > 데이터베이스명
-			// jdbc :oracle:thin: @//[HOST]   [:PORT] /SERVICE
-			String 주소 = "jdbc:mariadb://localhost:3306/edu";
-			String 사용자 = "root";
-			String 비밀번호 = "1234";
-			DriverManager.getConnection(주소, 사용자, 비밀번호);
-			System.out.println("섹션 생성 완료!");
+			String sql1 = "create OR REPLACE table test (no int, name varchar(50))";
+			
+			PreparedStatement ps = conn.prepareStatement(sql1);
+			ps.execute();
+			
+			String sql2 = "insert into test (name, no) value ('하이', 1)";
+			
+			ps = conn.prepareStatement(sql2);
+			int result = ps.executeUpdate();
+			System.out.println(result);
+			
+			conn.close();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
