6c39bbb0af54a64eef0d53257247439f86fd8272
ShellFolder <kk1593@naver.com>
2024-05-27 16:09:47 +0900

    JDBC 문제

---
+++ src/java0527/TestDTO.java
@@ -0,0 +1,27 @@
+package java0527;
+
+public class TestDTO {
+	
+	private int no;
+	private String name;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "TestDTO [no=" + no + ", name=" + name + "]";
+	}
+	
+	
+
+}
