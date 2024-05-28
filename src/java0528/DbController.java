60b79055b830c1b8189d597d93f6e810ac6b4af3
ShellFolder <kk1593@naver.com>
2024-05-28 10:17:07 +0900

    JDBC 라이버리 클래스 만들기 1단계

--- src/java0528/DbController.java
+++ src/java0528/DbController.java
@@ -1,5 +1,47 @@
 package java0528;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbController {
+	
+	private String 드라이버;
+	private String 주소;
+	private String 사용자;
+	private String 비밀번호;
+	
+	public DbController(String 드라이버, String 주소, String 사용자, String 비밀번호) {
+		this.드라이버 = 드라이버;
+		this.주소 = 주소;
+		this.사용자 = 사용자;
+		this.비밀번호 = 비밀번호;
+		시작();
+	}
+	
+	private void 시작() {
+		try {
+			Class.forName(드라이버);
+			Connection conn = DriverManager.getConnection(주소, 사용자, 비밀번호);
+			
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void 테이블생성(String sql, Connection conn) {
+		
+	}
+	
+	private void 데이터읽기(String sql, Connection conn) {
+		
+	}
+	
+	private void cud기능(String sql, Connection conn) {
+		
+	}
 
 }
--- src/java0528/Study01.java
+++ src/java0528/Study01.java
@@ -4,6 +4,12 @@ public class Study01 {
 
 	public static void main(String[] args) {
 
+		String 드라이버 = "org.mariadb.jdbc.Driver";
+		String 주소 = "jdbc:mariadb://localhost:3306/edu";
+		String 사용자 = "root";
+		String 비밀번호 = "1234";
+		new DbController(드라이버, 주소, 사용자, 비밀번호);
+		
 		
 		
 	}
