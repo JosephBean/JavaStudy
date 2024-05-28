c1f6c737f0f136db2cd74354a4ae9bb9cc53a558
ShellFolder <kk1593@naver.com>
2024-05-28 10:52:42 +0900

    JDBC 라이버리 클래스 만들기 2단계

--- src/java0528/DbController.java
+++ src/java0528/DbController.java
@@ -2,6 +2,7 @@ package java0528;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DbController {
@@ -23,7 +24,7 @@ public class DbController {
 		try {
 			Class.forName(드라이버);
 			Connection conn = DriverManager.getConnection(주소, 사용자, 비밀번호);
-			
+			기능제어(conn);
 			conn.close();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -32,8 +33,28 @@ public class DbController {
 		}
 	}
 	
+	private void 기능제어(Connection conn) {
+		System.out.println("테이블 생할 합니다.");
+		String sql = "create OR REPLACE table 명단 (";
+		sql += " 번호 	int,";
+		sql += " 이름 	varchar(50),";
+		sql += " 성별 	varchar(50),";
+		sql += " 특징 	varchar(50),";
+		sql += " 해적단 	varchar(50),";
+		sql += " 역활		varchar(50)";
+		sql += " )";
+		테이블생성(sql, conn);
+	}
+	
 	private void 테이블생성(String sql, Connection conn) {
-		
+		System.out.println(sql);
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.execute();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private void 데이터읽기(String sql, Connection conn) {
