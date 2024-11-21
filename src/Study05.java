094ba37ee651943c6b2811350962c2503c742b13
JosephBean <kk1593@naver.com>
2024-11-21 16:03:33 +0900

    반복문

---
+++ src/Study05.java
@@ -0,0 +1,30 @@
+public class Study05 {
+    
+    public static void main(String[] args) {
+        
+        int a = 3;
+
+        switch(a) {
+            case 1 :
+                System.out.println(1);
+                break;
+            case 2 :
+                System.out.println(2);
+                break;
+            case 3 :
+                System.out.println(3);
+                break;
+            case 4 :
+                System.out.println(4);
+                break;
+            case 5 :
+                System.out.println(5);
+                break;
+            default:
+                System.out.println("그 외의 값");
+                
+        }
+
+    }
+
+}
---
+++ src/Study06.java
@@ -0,0 +1,11 @@
+public class Study06 {
+    public static void main(String[] args) {
+        // int 단 = 1;
+
+        for(int 단 = 1; 단 <= 9; 단++) {
+            for(int i = 1; i <= 9; i++) {
+                System.out.println(단 + " * " + i + " = " + (단 * i) );
+            }
+        }
+    }
+}
