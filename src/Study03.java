f5f946ee3b01027065ae4ae253cf2634be1ccffa
JosephBean <kk1593@naver.com>
2024-11-21 15:07:41 +0900

    비교자 내용 추가

---
+++ src/Study03.java
@@ -0,0 +1,23 @@
+public class Study03 {
+    
+    public static void main(String[] args) {
+        
+        // 계산기
+        int a = 5;
+        int b = 3;
+
+        int 합 = (a + b);       // 5 + 3 = 8
+        int 빼기 = (a - b);     // 5 - 3 = 2
+        int 곱 = (a * b);       // 5 * 3 = 15
+        int 나누기 = (a / b);   // 5 / 3 = 1
+        int 나머지 = (a % b);   // 5 % 3 = 2
+
+        System.out.println(합);
+        System.out.println(빼기);
+        System.out.println(곱);
+        System.out.println(나누기);
+        System.out.println(나머지);
+
+    }
+
+}
---
+++ src/Study04.java
@@ -0,0 +1,12 @@
+public class Study04 {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        
+        if(a == b) {
+            System.out.println("참");
+        }
+    }
+
+}
