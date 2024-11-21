440c308960c366e61004422614e68a0b9cc96062
JosephBean <kk1593@naver.com>
2024-11-21 15:40:51 +0900

    조건문1

--- src/Study04.java
+++ src/Study04.java
@@ -4,9 +4,35 @@ public class Study04 {
         int a = 5;
         int b = 3;
         
-        if(a == b) {
+        // if(a == b) { // a와 b의 값이 같으면 참으로 출력하기
+        //     System.out.println("참");
+        // }
+
+        // if(a != b) { // a와 b의 값이 같지 않으면 참으로 출력하기
+        //     System.out.println("참");
+        // }
+
+        // boolean c = true;
+        // if(c) {
+        //     System.out.println("참");
+        // } else {
+        //     System.out.println("거짓");
+        // }
+
+        // System.out.println((a == b));
+        // System.out.println((a != b));
+
+
+        if(a == b) { // a= 5, b= 3
+            
+        } else if(a < b) {
+
+        } else if(a <= b) {
+
+        } else {
             System.out.println("참");
         }
+
     }
 
 }
