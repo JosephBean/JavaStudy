d9c3af703631016eca213b1c5ea5066c41cd769d
JosephBean <kk1593@naver.com>
2024-11-21 14:18:28 +0900

    생성자 사용하여 출력하기

---
+++ src/Study01.java
@@ -0,0 +1,14 @@
+public class Study01 {
+
+    public static void main(String[] ar) {
+        Study01 study = new Study01();
+        System.out.println(study);
+        int a = study.일의_값을_전달_받는_메소드();
+        System.out.println(a);
+    }
+
+    public int 일의_값을_전달_받는_메소드() {
+        return 1;
+    }
+
+}
