6901687ae9ea46d35e533a951de264aa9b0f5880
JosephBean <kk1593@naver.com>
2024-11-22 15:42:50 +0900

    배열 문제3번

--- src/Study12.java
+++ src/Study12.java
@@ -4,7 +4,8 @@ public class Study12 {
     public static void main(String[] args) {
         Study12 클래스 = new Study12();
         // 클래스.배열확인(클래스.문제1값());
-        클래스.배열확인(클래스.문제2값());
+        // 클래스.배열확인(클래스.문제2값());
+        클래스.배열확인(클래스.문제3값());
     }
 
     private String[][] 문제1값() {
@@ -40,6 +41,23 @@ public class Study12 {
             }
         }
         return 문제2;
+    }
+
+    private String[][] 문제3값() {
+        String[][] 문제3 = new String[10][10];
+        for(int i = 0; i < 문제3.length; i++) {
+            for(int j = 0; j < 문제3[i].length; j++) {
+                // 0 ~ 8   < 9   (10 - 1) 행 = 0
+                // 0 ~ 7   < 8   (10 - 2) 행 = 1
+                // 0 ~ 6   < 7   (10 - 3) 행 = 2
+                if(j < 문제3[i].length - (i+1)) {
+                    문제3[i][j] = 빈;
+                } else {
+                    문제3[i][j] = o;
+                }
+            }
+        }
+        return 문제3;
     }
 
     private void 배열확인(String[][] 배열) {
