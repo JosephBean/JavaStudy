1c0bd16d976013a32d75d6fe32e4b4b5504a81b3
JosephBean <kk1593@naver.com>
2024-11-25 14:50:39 +0900

    상속 & 생성자 문제

--- src/Study17.java
+++ src/Study17.java
@@ -1,19 +1,29 @@
 public class Study17 {
     public static void main(String[] args) {
-        new S17A().run();
+        S17A s17a = new S17A();
+        S17B s17b = new S17B();
+        // s17a.run();
+        // s17b.run();
+        s17b.call(s17a, s17b);
     }
 }
 
 class S17AB {
     public void run() {
-        System.out.println("안녕하세요.");
+        System.out.println(this + " 안녕하세요.");
     }
 }
 
 class S17A extends S17AB {
-
+    public void call(S17A s17a, S17B s17b) {
+        s17a.run();
+        s17b.call(s17a, s17b);
+    }
 }
 
 class S17B extends S17AB {
-
+    public void call(S17A s17a, S17B s17b) {
+        s17b.run();
+        s17a.call(s17a, s17b);
+    }
 }
