e734d728a6bcd08db2454abec3a56a9424ed1fa1
JosephBean <kk1593@naver.com>
2024-11-26 10:50:52 +0900

    리스트 실습하기

--- src/Study20.java
+++ src/Study20.java
@@ -1,5 +1,18 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Study20 {
     public static void main(String[] args) {
+        List list = new ArrayList<>();
+        list.add(1);    //  0
+        list.add("1");  //  1
+        list.add(true); //  2
+
+        System.out.println(list.size());
         
+        list.remove(0);
+        System.out.println(list.size());
+        System.out.println( list.get(0) instanceof String );
     }
 }
