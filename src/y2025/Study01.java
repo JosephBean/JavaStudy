4f8ee8afaea7eb9bfa36c58fefcc6ec1c8f321ce
JosephBean <kk1593@naver.com>
2025-01-08 09:44:04 +0900

    상속 테스트 1

--- src/y2025/Study01.java
+++ src/y2025/Study01.java
@@ -3,8 +3,25 @@ package y2025;
 public class Study01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		I i = (System.out::println);
+		I a1 = new A();
+		A a2 = new A();
+		I b1 = new B();
+		A b2 = new B();
+		i.print("동물 소리"); // 1
+		a1.print("야옹");    // 2
+		a2.print("멍멍");    // 3
+		b1.print("꼬꼬");    // 4
+		b2.print("꿀꿀");    // 5
 	}
 
+}
+interface I {
+	void print(String txt);
+}
+class A implements I {
+	public void print(String txt) {System.out.println("A : " + txt);}
+}
+class B extends A {
+	public void print(String txt) {System.out.println("B : " + txt);}
 }
